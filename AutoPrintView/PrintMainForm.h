d598bafb5c5af67f72a2b7896bc8b5d663fb1063
FrancisPGP <a20203647@pucp.edu.pe>
2024-01-25 15:15:31 -0500

    ACTUALIZACIÓN

--- AutoPrintView/PrintMainForm.h
+++ AutoPrintView/PrintMainForm.h
@@ -48,11 +48,17 @@ namespace AutoPrintView {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"PrintMainForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// PrintMainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(512, 402);
+			this->Name = L"PrintMainForm";
+			this->Text = L"PrintMainForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
